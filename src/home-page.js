5eac8ba1868fed70f163b212cc5ee2957675138f
stefan <stefankaraferovic@gmail.com>
2020-07-25 00:25:35 +0200

    Slogan effect

--- src/home-page.js
+++ src/home-page.js
@@ -1,7 +1,19 @@
 const renderHomePage = (() => {
+    // container
     const container = document.querySelector('#page-container');
-    const  render = () => {
-        //nav menu
+    let slogan;
+    const _deletePage = () => {
+        let child = container.lastChild;
+        while(child){
+            container.removeChild(child);
+            child = container.lastChild;
+        }
+    }
+
+    const render = () => {
+        _deletePage();
+        
+        // nav menu
         const navMenu = document.createElement('nav');
         navMenu.setAttribute('id', 'main-nav');
         const ul = document.createElement('ul');
@@ -12,28 +24,68 @@ const renderHomePage = (() => {
         li1.setAttribute('id', 'logo-li');
         li1.appendChild(imgLogo);
         const li2 = document.createElement('li');
-        li1.textContent = 'Home';
+        li2.textContent = 'Home';
         const li3 = document.createElement('li');
-        li1.textContent = 'Menu';
+        li3.textContent = 'Menu';
         const li4 = document.createElement('li');
-        li1.textContent = 'About';
+        li4.textContent = 'About';
         const li5 = document.createElement('li');
-        li1.textContent = 'Contact';
+        li5.textContent = 'Contact';
         ul.appendChild(li1);
         ul.appendChild(li2);
         ul.appendChild(li3);
         ul.appendChild(li4);
         ul.appendChild(li5);
-
-
-        //content
+        navMenu.appendChild(ul);
+        
+        // content
         const content = document.createElement('div');
         content.setAttribute('id', 'content');
+        slogan = document.createElement('h1');
+        slogan.setAttribute('id', 'slogan');
+        slogan.textContent = 'A tasty burger is what you deserve';
+        content.appendChild(slogan);
+
+        // footer
+        const footer = document.createElement('footer');
+        const footerDiv = document.createElement('div');
+        footerDiv.innerHTML = `Copyright &copy;
+        <script>document.write(new Date().getFullYear());</script>&nbsp;&nbsp;Created by Stefan Karaferovic
+        | <i class="fab fa-github"></i> <a href="https://github.com/stefank-29" target="_blank">stefank-29</a>`;
+        footer.appendChild(footerDiv);
+
+        // append
+        container.appendChild(navMenu);
+        container.appendChild(content);
+        container.appendChild(footer);
         
     }
-
+    const body = document.querySelector('body');
+    const walk = 30;
+    let xWalk;
+    let yWalk;
+    function shadow(e) {   
+        // offseti
+        let width = body.offsetWidth;
+        let height = body.offsetHeight;
+        let x = e.offsetX;
+        let y = e.offsetY;
+        if(this !== e.target){
+            x += e.target.offsetLeft;
+            y += e.target.offsetTop;
+        }
+        // walk je raspon
+        const xWalk = (x / width * walk) - (walk / 2);
+        const yWalk = (y / height * walk) - (walk / 2);
+        
+        slogan.style.boxShadow = `${xWalk}px ${yWalk}px 2px 5px rgba(0, 0, 0, 0.2)`;
+        
+        console.log("asd");
+    }
+    
+    body.addEventListener('mousemove', shadow);
     return {
-        render
+        render,
     }
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 //TODO efekti za slogan
 //TODO ikonice za fejs, insta gore desno
 //TODO tabs 
+
+import {renderHomePage} from './home-page'
+
+window.onload = renderHomePage.render();
