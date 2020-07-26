c2e60e609b34cd205a563129a41e57da5701e183
stefan <stefankaraferovic@gmail.com>
2020-07-26 10:22:54 +0200

    Menu hover and menu item hover

--- src/index.js
+++ src/index.js
@@ -5,16 +5,8 @@
 //TODO ikonice za fejs, insta gore desno
 //TODO Favicon
 
-import {renderHomePage} from './home-page'
+import {renderHomePage} from './home-page';
 import {renderMenuPage} from './menu-page';
 
 window.onload = renderHomePage.render();
 
-// function a (){
-    
-// }
-// const home = document.querySelector("#home-li");
-// home.addEventListener('click', renderHomePage.render);
-
-// const menu = document.querySelector('#menu-li');
-// menu.addEventListener('click', function(){renderMenuPage.render('burgers')});
