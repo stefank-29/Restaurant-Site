a070f81bb44dd4a29c307384d14bbf79a044d6d5
stefan <stefankaraferovic@gmail.com>
2020-07-26 14:46:51 +0200

    All images inserted

--- src/menu-page.js
+++ src/menu-page.js
@@ -131,12 +131,16 @@ const renderMenuPage = (() => {
 
             itemHeader2.appendChild(name2);
             itemHeader2.appendChild(price2);
+            //img
+            const img2 = document.createElement('img');
+            img2.setAttribute('src', '../images/burgers/burger2.jpg');
 
             const itemDescription2 = document.createElement('div');
             itemDescription2.classList.add('menu-item-description');
             const p2 = document.createElement('p');
             p2.innerHTML = 'Lettuce, Tomato, Pickles, Kewpie Mayo <br> Choose from: Nashville Hot Spice Blend, Honey Garlic BBQ, or Buffalo';
             itemDescription2.appendChild(p2);
+            itemDescription2.appendChild(img2);
 
             item2.appendChild(itemHeader2);
             item2.appendChild(itemDescription2);
@@ -155,12 +159,17 @@ const renderMenuPage = (() => {
 
             itemHeader3.appendChild(name3);
             itemHeader3.appendChild(price3);
+            //img
+            const img3 = document.createElement('img');
+            img3.setAttribute('src', '../images/burgers/burger3.jpg');
 
             const itemDescription3 = document.createElement('div');
             itemDescription3.classList.add('menu-item-description');
             const p3 = document.createElement('p');
             p3.textContent = 'Hoisin Glazed Pork Belly and Burger Patty, Gochujang Aioli, Fried Shallots, Cabbage, Cilantro';
             itemDescription3.appendChild(p3);
+            itemDescription3.appendChild(img3);
+            
 
             item3.appendChild(itemHeader3);
             item3.appendChild(itemDescription3);
@@ -180,12 +189,17 @@ const renderMenuPage = (() => {
 
             itemHeader4.appendChild(name4);
             itemHeader4.appendChild(price4);
+            //img
+            const img4 = document.createElement('img');
+            img4.setAttribute('src', '../images/burgers/burger4.jpg');
+
 
             const itemDescription4 = document.createElement('div');
             itemDescription4.classList.add('menu-item-description');
             const p4 = document.createElement('p');
             p4.textContent = 'Lettuce, Tomatoes, Pickle, Cheddar, Fry Sauce';
             itemDescription4.appendChild(p4);
+            itemDescription4.appendChild(img4);
 
             item4.appendChild(itemHeader4);
             item4.appendChild(itemDescription4);
@@ -204,12 +218,17 @@ const renderMenuPage = (() => {
 
             itemHeader5.appendChild(name5);
             itemHeader5.appendChild(price5);
+            //img
+            const img5 = document.createElement('img');
+            img5.setAttribute('src', '../images/burgers/burger5.jpg');
+
 
             const itemDescription5 = document.createElement('div');
             itemDescription5.classList.add('menu-item-description');
             const p5 = document.createElement('p');
             p5.textContent = 'Beer Battered Cod, Lettuce, Tomato, Cajun Remoulade, Pickles';
             itemDescription5.appendChild(p5);
+            itemDescription5.appendChild(img5);
 
             item5.appendChild(itemHeader5);
             item5.appendChild(itemDescription5);
@@ -228,12 +247,17 @@ const renderMenuPage = (() => {
 
             itemHeader6.appendChild(name6);
             itemHeader6.appendChild(price6);
+            //img
+            const img6 = document.createElement('img');
+            img6.setAttribute('src', '../images/burgers/burger6.jpg');
+
 
             const itemDescription6 = document.createElement('div');
             itemDescription6.classList.add('menu-item-description');
             const p6 = document.createElement('p');
             p6.textContent = 'Ginger Garlic Marinated Chicken Thigh, Cucumber, Pickled Daikon, Pickled Carrots, Cilantro, Fresh Jalapeno, Kewpie Mayo';
             itemDescription6.appendChild(p6);
+            itemDescription6.appendChild(img6);
 
             item6.appendChild(itemHeader6);
             item6.appendChild(itemDescription6);
@@ -252,12 +276,17 @@ const renderMenuPage = (() => {
 
             itemHeader7.appendChild(name7);
             itemHeader7.appendChild(price7);
+            //img
+            const img7 = document.createElement('img');
+            img7.setAttribute('src', '../images/burgers/burger7.jpg');
+
 
             const itemDescription7 = document.createElement('div');
             itemDescription7.classList.add('menu-item-description');
             const p7 = document.createElement('p');
             p7.textContent = 'Shallot Peppered Sirloin, Cucumber, Pickled Daikon, Pickled Carrots, Cilantro, Fresh Jalapeno, Kewpie Mayo';
             itemDescription7.appendChild(p7);
+            itemDescription7.appendChild(img7);
 
             item7.appendChild(itemHeader7);
             item7.appendChild(itemDescription7);
@@ -277,12 +306,17 @@ const renderMenuPage = (() => {
 
             itemHeader8.appendChild(name8);
             itemHeader8.appendChild(price8);
+            //img
+            const img8 = document.createElement('img');
+            img8.setAttribute('src', '../images/burgers/burger8.jpg');
+
 
             const itemDescription8 = document.createElement('div');
             itemDescription8.classList.add('menu-item-description');
             const p8 = document.createElement('p');
             p8.textContent = 'Hoisin Sambal Glazed Belly, Cucumber, Pickled Daikon, Pickled Carrots, Cilantro, Fresh Jalapeno, Kewpie Mayo';
             itemDescription8.appendChild(p8);
+            itemDescription8.appendChild(img8);
 
             item8.appendChild(itemHeader8);
             item8.appendChild(itemDescription8);
@@ -325,12 +359,15 @@ const renderMenuPage = (() => {
       
                   itemHeader1.appendChild(name1);
                   itemHeader1.appendChild(price1);
+                  const img1 = document.createElement('img');
+                  img1.setAttribute('src', '../images/snacks/snack1.jpg');
       
                   const itemDescription1 = document.createElement('div');
                   itemDescription1.classList.add('menu-item-description');
                   const p1 = document.createElement('p');
                   p1.textContent = 'Served with Fry Sauce';
                   itemDescription1.appendChild(p1);
+                  itemDescription1.appendChild(img1);-
       
                   item1.appendChild(itemHeader1);
                   item1.appendChild(itemDescription1);
@@ -349,12 +386,15 @@ const renderMenuPage = (() => {
       
                   itemHeader2.appendChild(name2);
                   itemHeader2.appendChild(price2);
+                  const img2 = document.createElement('img');
+                  img2.setAttribute('src', '../images/snacks/snack2.jpg');
       
                   const itemDescription2 = document.createElement('div');
                   itemDescription2.classList.add('menu-item-description');
                   const p2 = document.createElement('p');
                   p2.innerHTML = 'Choose from: Buffalo, Honey Garlic BBQ, Hoisin Sambal, or Gochujang Honey. Served with Celery & Buttermilk Ranch or Blue Cheese';
                   itemDescription2.appendChild(p2);
+                  itemDescription2.appendChild(img2);
       
                   item2.appendChild(itemHeader2);
                   item2.appendChild(itemDescription2);
@@ -373,12 +413,15 @@ const renderMenuPage = (() => {
       
                   itemHeader3.appendChild(name3);
                   itemHeader3.appendChild(price3);
-      
+                  const img3 = document.createElement('img');
+                  img3.setAttribute('src', '../images/snacks/snack3.jpg');
+
                   const itemDescription3 = document.createElement('div');
                   itemDescription3.classList.add('menu-item-description');
                   const p3 = document.createElement('p');
                   p3.textContent = 'Choose from: Buffalo, Honey Garlic BBQ, Hoisin Sambal, Gochujang Honey, Buttermilk Ranch, or Blue Cheese.';
                   itemDescription3.appendChild(p3);
+                  itemDescription3.appendChild(img3);
       
                   item3.appendChild(itemHeader3);
                   item3.appendChild(itemDescription3);
@@ -398,12 +441,15 @@ const renderMenuPage = (() => {
       
                   itemHeader4.appendChild(name4);
                   itemHeader4.appendChild(price4);
+                  const img4 = document.createElement('img');
+                  img4.setAttribute('src', '../images/snacks/snack4.jpg');
       
                   const itemDescription4 = document.createElement('div');
                   itemDescription4.classList.add('menu-item-description');
                   const p4 = document.createElement('p');
                   p4.textContent = 'Roasted Sweet Corn, Pico de Gallo, Cilantro Lime Crema, Tajin, Cotija Cheese';
                   itemDescription4.appendChild(p4);
+                  itemDescription4.appendChild(img4);
       
                   item4.appendChild(itemHeader4);
                   item4.appendChild(itemDescription4);
@@ -422,12 +468,15 @@ const renderMenuPage = (() => {
       
                   itemHeader5.appendChild(name5);
                   itemHeader5.appendChild(price5);
+                  const img5 = document.createElement('img');
+                  img5.setAttribute('src', '../images/snacks/snack5.jpg');
       
                   const itemDescription5 = document.createElement('div');
                   itemDescription5.classList.add('menu-item-description');
                   const p5 = document.createElement('p');
                   p5.textContent = 'Beer Battered Cod, Cabbage, Pico de Gallo, Guacamole, Cilantro Lime Crema';
                   itemDescription5.appendChild(p5);
+                  itemDescription5.appendChild(img5);
       
                   item5.appendChild(itemHeader5);
                   item5.appendChild(itemDescription5);
@@ -446,12 +495,15 @@ const renderMenuPage = (() => {
       
                   itemHeader6.appendChild(name6);
                   itemHeader6.appendChild(price6);
+                  const img6 = document.createElement('img');
+                  img6.setAttribute('src', '../images/snacks/snack6.jpg');
       
                   const itemDescription6 = document.createElement('div');
                   itemDescription6.classList.add('menu-item-description');
                   const p6 = document.createElement('p');
                   p6.textContent = 'Bacon Wrapped All Beef Dog, Grilled Fajitas, Cilantro Lime Crema, Cotija Cheese';
                   itemDescription6.appendChild(p6);
+                  itemDescription6.appendChild(img6);
       
                   item6.appendChild(itemHeader6);
                   item6.appendChild(itemDescription6);
@@ -470,12 +522,15 @@ const renderMenuPage = (() => {
       
                   itemHeader7.appendChild(name7);
                   itemHeader7.appendChild(price7);
+                  const img7 = document.createElement('img');
+                  img7.setAttribute('src', '../images/snacks/snack7.jpg');
       
                   const itemDescription7 = document.createElement('div');
                   itemDescription7.classList.add('menu-item-description');
                   const p7 = document.createElement('p');
                   p7.textContent = 'Pork Belly & Chicken with a House Cheese Blend, Pico De Gallo, Guacamole, Cilantro Lime Crema';
                   itemDescription7.appendChild(p7);
+                  itemDescription7.appendChild(img7);
       
                   item7.appendChild(itemHeader7);
                   item7.appendChild(itemDescription7);
@@ -518,12 +573,15 @@ const renderMenuPage = (() => {
       
                   itemHeader1.appendChild(name1);
                   itemHeader1.appendChild(price1);
+                  const img1 = document.createElement('img');
+                  img1.setAttribute('src', '../images/salads/salad1.jpg');
       
                   const itemDescription1 = document.createElement('div');
                   itemDescription1.classList.add('menu-item-description');
                   const p1 = document.createElement('p');
                   p1.textContent = 'Carrots, Tomatoes, Cucumber, Croutons & Choice of: Citrus Vinaigrette (V), Ranch, or Blue Cheese Dressing';
                   itemDescription1.appendChild(p1);
+                  itemDescription1.appendChild(img1);
       
                   item1.appendChild(itemHeader1);
                   item1.appendChild(itemDescription1);
@@ -542,12 +600,16 @@ const renderMenuPage = (() => {
       
                   itemHeader2.appendChild(name2);
                   itemHeader2.appendChild(price2);
+                  const img2 = document.createElement('img');
+                  img2.setAttribute('src', '../images/salads/salad2.jpg');
+      
       
                   const itemDescription2 = document.createElement('div');
                   itemDescription2.classList.add('menu-item-description');
                   const p2 = document.createElement('p');
                   p2.innerHTML = 'Romaine, Carrots, Tomatoes, Cucumber, Egg, Bacon, Blue Cheese';
                   itemDescription2.appendChild(p2);
+                  itemDescription2.appendChild(img2);
       
                   item2.appendChild(itemHeader2);
                   item2.appendChild(itemDescription2);
@@ -566,12 +628,16 @@ const renderMenuPage = (() => {
       
                   itemHeader3.appendChild(name3);
                   itemHeader3.appendChild(price3);
+                  const img3 = document.createElement('img');
+                  img3.setAttribute('src', '../images/salads/salad3.jpg');
+      
       
                   const itemDescription3 = document.createElement('div');
                   itemDescription3.classList.add('menu-item-description');
                   const p3 = document.createElement('p');
                   p3.textContent = 'Romaine, Shaved Parmesan, Croutons';
                   itemDescription3.appendChild(p3);
+                  itemDescription3.appendChild(img3);
       
                   item3.appendChild(itemHeader3);
                   item3.appendChild(itemDescription3);
@@ -642,12 +708,16 @@ const renderMenuPage = (() => {
       
                   itemHeader1.appendChild(name1);
                   itemHeader1.appendChild(price1);
+                  const img1 = document.createElement('img');
+                  img1.setAttribute('src', '../images/minies/mini1.jpg');
+      
       
                   const itemDescription1 = document.createElement('div');
                   itemDescription1.classList.add('menu-item-description');
                   const p1 = document.createElement('p');
                   p1.textContent = 'Meat, Cheese, Potato Roll';
                   itemDescription1.appendChild(p1);
+                  itemDescription1.appendChild(img1);
       
                   item1.appendChild(itemHeader1);
                   item1.appendChild(itemDescription1);
@@ -666,12 +736,15 @@ const renderMenuPage = (() => {
       
                   itemHeader2.appendChild(name2);
                   itemHeader2.appendChild(price2);
+                  const img2 = document.createElement('img');
+                  img2.setAttribute('src', '../images/minies/mini2.jpg');
       
                   const itemDescription2 = document.createElement('div');
                   itemDescription2.classList.add('menu-item-description');
                   const p2 = document.createElement('p');
-                  p2.innerHTML = 'American & Cheddar';
+                  p2.textContent = 'American & Cheddar';
                   itemDescription2.appendChild(p2);
+                  itemDescription2.appendChild(img2);
       
                   item2.appendChild(itemHeader2);
                   item2.appendChild(itemDescription2);
@@ -690,8 +763,19 @@ const renderMenuPage = (() => {
       
                   itemHeader3.appendChild(name3);
                   itemHeader3.appendChild(price3);
+                  const img3 = document.createElement('img');
+                  img3.setAttribute('src', '../images/minies/mini3.jpg');
+
+                  const itemDescription3 = document.createElement('div');
+                  itemDescription3.classList.add('menu-item-description');
+                  const p3 = document.createElement('p');
+                  p3.innerHTML = '';
+                  itemDescription3.appendChild(p3);
+                  itemDescription3.appendChild(img3);
         
                   item3.appendChild(itemHeader3);
+                  item3.appendChild(itemDescription3);
+                 
                 // -----------------------------------
       
                    menuItems.appendChild(item1);
@@ -753,7 +837,7 @@ const renderMenuPage = (() => {
          console.log('x:' + xWalk, 'y:' +yWalk);
       //  item.style.boxShadow = `${xWalk}px ${yWalk}px 2px 5px red`;
         let img = item.querySelector('img');
-        img.style.transform = `translateX(${xWalk/75+55}rem) translateY(${yWalk/75-15}rem)`;
+        img.style.transform = `translateX(${xWalk/75+55}rem) translateY(${yWalk/75-18}rem)`;
     }
 
     return{
