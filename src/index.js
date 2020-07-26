c6f2ac157455256d13ae61e33419967469168271
stefan <stefankaraferovic@gmail.com>
2020-07-26 21:34:36 +0200

    Img fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 //TODO about
 //TODO Contact (google maps)
-//TODO google maps dark mode
+//TODO google maps dark
 //TODO Home page dodati jos nesto
 //TODO ikonice za fejs, insta gore desno
 //TODO Favicon
+//git subtree push --prefix dist origin gh-pages
+
 
 import {renderHomePage} from './home-page';
 import {renderMenuPage} from './menu-page';
 
-window.onload = renderHomePage.render();
+//window.onload = renderHomePage.render();
 
